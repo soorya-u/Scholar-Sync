4fc4217ce7c61419e7c0455bc07b86f2cf0c3440
Soorya U <sooryau7@gmail.com>
2024-05-25 20:42:53 +0530

    feat: Added Tanstack Router to the Project
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+      </div>
+      <hr />
+      <Outlet />
+      <React.Suspense>
+        <TanStackRouterDevtools />
+      </React.Suspense>
+    </>
+  ),
+});
