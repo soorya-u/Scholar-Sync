19517084f6b97f836e6fd7b19b78d4b5abe259c9
Soorya U <sooryau7@gmail.com>
2024-05-30 20:54:08 +0530

    feat: Added Login Query Resolver
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- server/helpers/jwt.go
+++ server/helpers/jwt.go
@@ -25,3 +25,32 @@ func GenerateJWT(userId string) (string, error) {
 	return jwtString, err
 
 }
+
+func DecodeJWT(jwtString string) (string, error) {
+
+	token, err := jwt.Parse(jwtString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		jwtSecret := os.Getenv("JWT_SECRET")
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("unable to parse: %v", err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return "", fmt.Errorf("token has expired")
+		}
+
+		userId := claims["sub"].(string)
+
+		return userId, nil
+	} else {
+		return "", fmt.Errorf("unable to claim jwt")
+	}
+}
