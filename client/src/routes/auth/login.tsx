0cad73dc9f4276775d95da5f5d18970d2b677610
Soorya U <sooryau7@gmail.com>
2024-06-14 21:10:22 +0530

    feat: Added Barebones for Auth Pages
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/routes/auth/login.tsx
+++ client/src/routes/auth/login.tsx
@@ -1,4 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderCircle } from "lucide-react";
 
 import { Label } from "@/components/primitives/label";
 import { Button } from "@/components/primitives/button";
@@ -12,38 +15,99 @@ import {
 } from "@/components/primitives/card";
 import { Input } from "@/components/primitives/input";
 
+import { loginSchema, LoginType } from "@/schema/login";
+import { useToast } from "@/components/primitives/use-toast";
+
 export const Route = createFileRoute("/auth/login")({
   component: Login,
 });
 
 function Login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginType>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: "", password: "" },
+  });
+  const { toast } = useToast();
+
   return (
-    <div className="flex flex-1 justify-center items-center">
+    // TODO: Add a Header or Something
+    <div className="flex min-h-full justify-center items-center">
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-2xl font-lato">Login</CardTitle>
           <CardDescription>
             Enter your email below to login to your account.
           </CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Sign in</Button>
-        </CardFooter>
+        <form
+          onSubmit={handleSubmit((val) =>
+            toast({
+              title: "Registration Successfull!",
+              description: JSON.stringify(val),
+              variant: "destructive",
+            })
+          )}
+        >
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label className="font-lato font-bold" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                disabled={isSubmitting}
+                aria-disabled={isSubmitting}
+                {...register("email")}
+                id="email"
+                placeholder="john-doe@example.com"
+                required
+              />
+              <span className="text-red-500 text-xs px-2">
+                {errors && errors.email && errors.email.message}
+              </span>
+            </div>
+            <div className="grid gap-2">
+              <Label className="font-lato font-bold" htmlFor="password">
+                Password
+              </Label>
+              <Input
+                disabled={isSubmitting}
+                aria-disabled={isSubmitting}
+                {...register("password")}
+                id="password"
+                type="password"
+                placeholder="********"
+                required
+              />
+              <span className="text-red-500 text-xs px-2">
+                {errors && errors.password && errors.password.message}
+              </span>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <Button
+              disabled={isSubmitting}
+              aria-disabled={isSubmitting}
+              type="submit"
+              className="w-full"
+            >
+              {isSubmitting ? (
+                <LoaderCircle className="animate-spin" />
+              ) : (
+                "Sign in"
+              )}
+            </Button>
+            <div className="mt-4 text-center text-sm">
+              Do not have an account?{" "}
+              <Link to="/auth/sign-up" className="underline">
+                Sign Up
+              </Link>
+            </div>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );
--- client/src/routes/auth/sign-up.tsx
+++ client/src/routes/auth/sign-up.tsx
@@ -1,5 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/primitives/button";
 import {
@@ -12,56 +14,117 @@ import {
 import { Input } from "@/components/primitives/input";
 import { Label } from "@/components/primitives/label";
 
+import { SignUpType, signUpSchema } from "@/schema/sign-up";
+import { useToast } from "@/components/primitives/use-toast";
+
 export const Route = createFileRoute("/auth/sign-up")({
   component: SignUp,
 });
 
 export function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<SignUpType>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: { email: "", firstName: "", lastName: "", password: "" },
+  });
+  const { toast } = useToast();
+
   return (
-    <div className="flex flex-1 justify-center items-center">
+    <div className="flex min-h-full justify-center items-center">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
-          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardTitle className="text-xl font-lato">Sign Up</CardTitle>
           <CardDescription>
             Enter your information to create an account
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="grid gap-4">
-            <div className="grid grid-cols-2 gap-4">
+          <form
+            onSubmit={handleSubmit((val) =>
+              toast({
+                title: "Registration Successfull!",
+                description: JSON.stringify(val),
+                variant: "destructive",
+              })
+            )}
+            className="grid gap-4"
+          >
+            <div className="grid grid-cols-2 gap-y-2 gap-x-4">
               <div className="grid gap-2">
-                <Label htmlFor="first-name">First name</Label>
-                <Input id="first-name" placeholder="Max" required />
+                <Label className="font-lato font-bold" htmlFor="first-name">
+                  First name
+                </Label>
+                <Input
+                  disabled={isSubmitting}
+                  aria-disabled={isSubmitting}
+                  id="first-name"
+                  placeholder="John"
+                  {...register("firstName")}
+                />
               </div>
               <div className="grid gap-2">
-                <Label htmlFor="last-name">Last name</Label>
-                <Input id="last-name" placeholder="Robinson" required />
+                <Label className="font-lato font-bold" htmlFor="last-name">
+                  Last name
+                </Label>
+                <Input
+                  disabled={isSubmitting}
+                  aria-disabled={isSubmitting}
+                  id="last-name"
+                  placeholder="Doe"
+                  {...register("lastName")}
+                />
               </div>
+              <span className="text-red-500 text-xs px-2">
+                {errors && errors.firstName && errors.firstName.message}
+              </span>
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label className="font-lato font-bold" htmlFor="email">
+                Email
+              </Label>
               <Input
+                disabled={isSubmitting}
+                aria-disabled={isSubmitting}
                 id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
+                {...register("email")}
+                placeholder="john-doe@example.com"
               />
+              <span className="text-red-500 text-xs px-2">
+                {errors && errors.email && errors.email.message}
+              </span>
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Label className="font-lato font-bold" htmlFor="password">
+                Password
+              </Label>
+              <Input
+                disabled={isSubmitting}
+                aria-disabled={isSubmitting}
+                id="password"
+                {...register("password")}
+                placeholder={"********"}
+                type="password"
+              />
+              <span className="text-red-500 text-xs px-2">
+                {errors && errors.password && errors.password.message}
+              </span>
             </div>
-            <Button type="submit" className="w-full">
+            <Button
+              disabled={isSubmitting}
+              aria-disabled={isSubmitting}
+              type="submit"
+              className="w-full"
+            >
               Create an account
             </Button>
-            <Button variant="outline" className="w-full">
-              Sign up with GitHub
-            </Button>
-          </div>
+          </form>
           <div className="mt-4 text-center text-sm">
             Already have an account?{" "}
-            <Link href="#" className="underline">
-              Sign in
+            <Link to="/auth/login" className="underline">
+              Sign In
             </Link>
           </div>
         </CardContent>
