d7724fb2d14bb1a459835e89cc1579cacf8b88be
Soorya U <sooryau7@gmail.com>
2024-06-16 14:43:52 +0530

    chore: Seperate Components for File and Announcements
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/components/custom/Dashboard/index.tsx
+++ client/src/components/custom/Dashboard/index.tsx
@@ -1,6 +1,6 @@
-import { FileDown } from "lucide-react";
-
 import { Separator } from "@/components/primitives/separator";
+import FileViewer from "./FileViewer";
+import AnnouncementViewer from "./AnnouncementViewer";
 
 export default function Dashboard() {
   return (
@@ -13,52 +13,25 @@ export default function Dashboard() {
         </div>
       </div>
       <Separator />
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-4 items-center justify-between">
-          <div className="flex gap-4 items-center">
-            <img
-              src="https://static.vecteezy.com/system/resources/previews/021/608/790/non_2x/chatgpt-logo-chat-gpt-icon-on-black-background-free-vector.jpg"
-              className="size-10 rounded-full"
-            />
-            <h2 className="text-lg">Alia Bhatt has made a new Announcement</h2>
-          </div>
-          <h4 className="text-base justify-self-end">20/20/2000</h4>
-        </div>
-        <div className="flex flex-col pl-10 gap-2">
-          <h1 className="text-3xl">Some Random Heading</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim ipsum
+      <AnnouncementViewer
+        creator="Soorya U"
+        date={new Date()}
+        description="orem ipsum dolor sit, amet consectetur adipisicing elit. Enim ipsum
             consectetur laborum cupiditate odio, asperiores quod aspernatur illo
             ea nisi hic dolorum officia, inventore sint tenetur rerum! Itaque
-            quae soluta commodi vero ex. Odit, quaerat?
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-4 items-center justify-between">
-          <div className="flex gap-4 items-center">
-            <img
-              src="https://static.vecteezy.com/system/resources/previews/021/608/790/non_2x/chatgpt-logo-chat-gpt-icon-on-black-background-free-vector.jpg"
-              className="size-10 rounded-full"
-            />
-            <h2 className="text-lg">Alia Bhatt has Uploaded a new File</h2>
-          </div>
-          <h4 className="text-base justify-self-end">20/20/2000</h4>
-        </div>
-        <div className="flex flex-col pl-10 gap-3">
-          <h1 className="text-3xl">Some Random Heading</h1>
-          <div className="flex items-center gap-4">
-            <FileDown className="size-12" />
-            <h3 className="text-lg">File Name.txt</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim ipsum
+            quae soluta commodi vero ex. Odit, quaerat?"
+        title="New Annnouncement WOw"
+      />
+      <FileViewer
+        creator="Soorya U"
+        date={new Date()}
+        description="orem ipsum dolor sit, amet consectetur adipisicing elit. Enim ipsum
             consectetur laborum cupiditate odio, asperiores quod aspernatur illo
             ea nisi hic dolorum officia, inventore sint tenetur rerum! Itaque
-            quae soluta commodi vero ex. Odit, quaerat?
-          </p>
-        </div>
-      </div>
+            quae soluta commodi vero ex. Odit, quaerat?"
+        fileName="Myfile.txt"
+        title="New File WOw"
+      />
     </div>
   );
 }
