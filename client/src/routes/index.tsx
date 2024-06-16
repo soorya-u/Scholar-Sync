8570d0f475cd2aff7b2e9cfb808b8a4314528e0b
Soorya U <sooryau7@gmail.com>
2024-06-16 13:44:57 +0530

    feat: Added Uploader
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -5,6 +5,10 @@ import { createFileRoute } from "@tanstack/react-router";
 import NavBar from "@/components/custom/NavBar";
 import UsersList from "@/components/custom/UsersList";
 import Uploader from "@/components/custom/Uploader";
+import {
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/primitives/resizable";
 
 export const Route = createFileRoute("/")({
   component: Dashboard,
@@ -20,12 +24,15 @@ function Dashboard() {
         <div className="flex-1 bg-background w-full h-full">
           <NavBar />
           <div className="flex h-[91%] flex-1">
-            <div className="flex flex-col h-full flex-1">
-              <div className="flex-1 w-full">
+            <ResizablePanelGroup
+              className="flex flex-col h-full flex-1"
+              direction="vertical"
+            >
+              <ResizablePanel className="flex-1 w-full">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              </div>
+              </ResizablePanel>
               <Uploader />
-            </div>
+            </ResizablePanelGroup>
             <UsersList />
           </div>
         </div>
