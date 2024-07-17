473327953173c2f05543d256761408f7c7446227
Soorya U <sooryau7@gmail.com>
2024-07-17 14:17:05 +0530

    chore: Added Toast Provider
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { PropsWithChildren } from "react";
+
+import { Toaster as ToasterProvider } from "@/components/primitives/toaster";
+
 import ApolloGraphQLProvider from "./ApolloGraphQL";
 import ReduxProvider from "./Redux";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <ApolloGraphQLProvider>
-      <ReduxProvider>{children}</ReduxProvider>
+      <ReduxProvider>
+        {children}
+        <ToasterProvider />
+      </ReduxProvider>
     </ApolloGraphQLProvider>
   );
 }
