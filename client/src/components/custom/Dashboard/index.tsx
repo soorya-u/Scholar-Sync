79e5820d42c0f5d2cd2709e5a6bbe032474cb75a
Soorya U <sooryau7@gmail.com>
2024-07-23 15:34:57 +0530

    fix: Fixed UI parts
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/components/custom/Dashboard/index.tsx
+++ client/src/components/custom/Dashboard/index.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/primitives/separator";
 import FileViewer from "./FileViewer";
 import AnnouncementViewer from "./AnnouncementViewer";
 import { dateFormatter } from "@/utils/date-formatter";
+import Image from "next/image";
 
 export default function Dashboard() {
   const { nexus } = useNexus();
@@ -25,47 +26,56 @@ export default function Dashboard() {
   }, [nexus]);
 
   return (
-    <div className="flex h-full flex-1 flex-col gap-4 overflow-y-auto p-4">
-      <div className="flex justify-between">
-        <h1 className="font-kanit text-lg text-slate-500">
-          Enjoy your Time from here on
-        </h1>
-        <h1 className="hidden font-kanit text-base text-slate-500 lg:block">
-          {dateFormatter(new Date(nexus.createdAt))}
-        </h1>
-      </div>
-      <Separator className="opacity-25" />
+    <>
+      {dashboardList.length === 0 ? (
+        <div className="flex h-full flex-1 flex-col justify-center items-center gap-4 overflow-y-auto p-4">
+          <h1 className="text-3xl text-center font-playwrite text-amber-300">No Files or Announcements Found</h1>
+          <Image src="/not-found.png" alt="not found" height={150} width={150} />
+        </div>
+      ) : (
+        <div className="flex h-full flex-1 flex-col gap-4 overflow-y-auto p-4">
+          <div className="flex justify-between">
+            <h1 className="font-kanit text-lg text-slate-500">
+              Enjoy your Time from here on
+            </h1>
+            <h1 className="hidden font-kanit text-base text-slate-500 lg:block">
+              {dateFormatter(new Date(nexus.createdAt))}
+            </h1>
+          </div>
+          <Separator className="opacity-25" />
 
-      {dashboardList.map((d) => {
-        if (d.id.split(":")[0] === "announcement")
-          return (
-            <AnnouncementViewer
-              key={d.id}
-              creatorId={d.sentBy.id}
-              creator={d.sentBy.fullName}
-              date={new Date(d.timeStamp)}
-              // @ts-ignore
-              description={d.message}
-              title={d.title}
-            />
-          );
-        else if (d.id.split(":")[0] === "file")
-          return (
-            <FileViewer
-              key={d.id}
-              creatorId={d.sentBy.id}
-              creator={d.sentBy.fullName}
-              date={new Date(d.timeStamp)}
-              // @ts-ignore
-              description={d.description}
-              // @ts-ignore
-              fileUrl={d.fileUrl}
-              // @ts-ignore
-              fileName={d.fileName}
-              title={d.title}
-            />
-          );
-      })}
-    </div>
+          {dashboardList.map((d) => {
+            if (d.id.split(":")[0] === "announcement")
+              return (
+                <AnnouncementViewer
+                  key={d.id}
+                  creatorId={d.sentBy.id}
+                  creator={d.sentBy.fullName}
+                  date={new Date(d.timeStamp)}
+                  // @ts-ignore
+                  description={d.message}
+                  title={d.title}
+                />
+              );
+            else if (d.id.split(":")[0] === "file")
+              return (
+                <FileViewer
+                  key={d.id}
+                  creatorId={d.sentBy.id}
+                  creator={d.sentBy.fullName}
+                  date={new Date(d.timeStamp)}
+                  // @ts-ignore
+                  description={d.description}
+                  // @ts-ignore
+                  fileUrl={d.fileUrl}
+                  // @ts-ignore
+                  fileName={d.fileName}
+                  title={d.title}
+                />
+              );
+          })}
+        </div>
+      )}
+    </>
   );
 }
