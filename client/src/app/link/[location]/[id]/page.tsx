1eb24df0b4fa813f0a067dba066ae636429ca404
Soorya U <sooryau7@gmail.com>
2024-07-28 12:07:36 +0530

    fix: Fixed UI Overflow
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/app/link/[location]/[id]/page.tsx
+++ client/src/app/link/[location]/[id]/page.tsx
@@ -14,8 +14,8 @@ export default function JoinPage({ params }: { params: { location: string } }) {
           <img src={"/logo.png"} alt="Logo" width={50} height={50} />
           <h1 className="font-playwrite text-2xl text-primary">Scholar Sync</h1>
         </div>
-        <h1 className="text-center font-playwrite text-4xl text-amber-400 sm:text-5xl">
-          Adding you to <br /> the requested
+        <h1 className="text-wrap text-center font-playwrite text-4xl leading-[3.5rem] text-amber-400 sm:text-5xl">
+          Adding you to the requested
           {params.location === "c" ? "Core" : "Nexus"}....
         </h1>
       </div>
