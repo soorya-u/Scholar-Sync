fa6f69583fbd35e27cc00404c8c0f4400560cd71
Soorya U <sooryau7@gmail.com>
2024-07-28 12:53:36 +0530

    fix: Fixed API Base Path
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
 });
 
 type BodyType = {
