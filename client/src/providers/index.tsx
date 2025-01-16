6271ade0327cc7bfa11e4f725e3044098eb9194f
Soorya U <sooryau7@gmail.com>
2025-01-16 17:22:57 +0530

    chore: added redux provider
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -5,17 +5,18 @@ import { PropsWithChildren } from "react";
 import { Toaster as ToasterProvider } from "@/components/ui/toaster";
 
 import ApolloProvider from "./apollo";
+import ReduxProvider from "./redux";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <ApolloProvider>
-      {/* <ReduxProvider> */}
+      <ReduxProvider>
         <SidebarProvider>
           {children}
           <ToasterProvider />
         </SidebarProvider>
-      {/* </ReduxProvider> */}
+      </ReduxProvider>
     </ApolloProvider>
   );
 }
