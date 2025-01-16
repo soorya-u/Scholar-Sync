f9abe9df5cbd616e25d1aa9530c0bfa9ac00a28e
Soorya U <sooryau7@gmail.com>
2025-01-16 17:24:55 +0530

    chore: user type fix
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/hooks/use-user.ts
+++ client/src/hooks/use-user.ts
@@ -2,14 +2,14 @@ import { useSelector, useDispatch } from "react-redux";
 
 import type { RootState } from "@/redux/store";
 import { setUser as setUserFn, resetUser } from "@/redux/slices/user";
-import { UserType } from "@/types/api";
+import { TUser } from "@/types/api";
 
 export const useUser = () => {
   const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   return {
     user,
-    setUser: (user: UserType) => dispatch(setUserFn(user)),
+    setUser: (user: TUser) => dispatch(setUserFn(user)),
     resetUser: () => dispatch(resetUser()),
   };
 };
