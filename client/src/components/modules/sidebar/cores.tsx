aa4721f85532fc87a3154d6abba73f76102fb4ac
Soorya U <sooryau7@gmail.com>
2025-01-26 10:59:03 +0530

    feat: added core and nexus creation modals
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/components/modules/sidebar/cores.tsx
+++ client/src/components/modules/sidebar/cores.tsx
@@ -15,6 +15,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCore } from "@/hooks/use-core";
+import CreateCoreModal from "../modals/core";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 
 export default function CoreSwitcher({
   cores,
@@ -29,80 +31,91 @@ export default function CoreSwitcher({
 
   const { core: activeCore, setCoreById } = useCore();
 
-  console.log({ activeCore });
-
-  if (activeCore.id === "")
-    // TODO: Add a Modal trigger to this
+  if (!activeCore.id)
     return (
-      <SidebarMenu>
-        <SidebarMenuItem>
+      <SidebarMenu className="gap-2 mb-3">
+        <SidebarMenuItem className="ml-2 mt-2">
           <SidebarMenuButton
+            asChild
             size="lg"
             className="flex cursor-pointer items-center justify-center gap-2 rounded-md data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
           >
-            <div className="flex size-8 items-center justify-center rounded-md border border-primary bg-background">
-              <Plus className="size-4" />
-            </div>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-semibold">Add new Core</span>
-            </div>
+            <CreateCoreModal>
+              <div className="flex size-7 items-center justify-center rounded-md border bg-background">
+                <Plus className="size-4" />
+              </div>
+              <span className="truncate text-sm font-semibold leading-tight">
+                Add new Core
+              </span>
+            </CreateCoreModal>
           </SidebarMenuButton>
         </SidebarMenuItem>
       </SidebarMenu>
     );
 
   return (
-    <SidebarMenu>
+    <SidebarMenu className="gap-2">
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="disabled:opacity-100" asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <img src={activeCore.imageUrl} className="size-4" />
-              </div>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                  {activeCore.name}
-                </span>
-              </div>
-              <ChevronsUpDown className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            align="start"
-            side={isMobile ? "bottom" : "right"}
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="text-xs text-muted-foreground">
-              Cores
-            </DropdownMenuLabel>
-            {cores.map((core) => (
-              <DropdownMenuItem
-                key={core.id}
-                onClick={async () => await setCoreById(core.id)}
-                className="gap-2 p-2"
+        <Dialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="disabled:opacity-100" asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
-                <div className="flex size-6 items-center justify-center rounded-sm border">
-                  <img src={core.imageUrl} className="size-4 shrink-0" />
+                <div className="flex aspect-square size-8 items-center justify-center bg-transparent text-sidebar-primary-foreground">
+                  <img
+                    src={activeCore.imageUrl}
+                    className="size-full rounded-lg border-2 border-primary"
+                  />
                 </div>
-                {core.name}
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    {activeCore.name}
+                  </span>
+                </div>
+                <ChevronsUpDown className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              align="start"
+              side={isMobile ? "bottom" : "right"}
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="text-xs text-muted-foreground">
+                Cores
+              </DropdownMenuLabel>
+              {cores.map((core) => (
+                <DropdownMenuItem
+                  key={core.id}
+                  onClick={async () => await setCoreById(core.id)}
+                  className="gap-2 p-2"
+                >
+                  <div className="flex size-6 items-center justify-center">
+                    <img
+                      src={core.imageUrl}
+                      className="size-full rounded-md border-2 border-primary"
+                    />
+                  </div>
+                  {core.name}
+                </DropdownMenuItem>
+              ))}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="gap-2 p-2">
+                <DialogTrigger className="flex items-center justify-center gap-2">
+                  <div className="flex size-6 items-center justify-center rounded-md border bg-background">
+                    <Plus className="size-4" />
+                  </div>
+                  <div className="font-medium text-muted-foreground">
+                    Add new Core
+                  </div>
+                </DialogTrigger>
               </DropdownMenuItem>
-            ))}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 p-2">
-              <div className="flex size-6 items-center justify-center rounded-md border bg-background">
-                <Plus className="size-4" />
-              </div>
-              <div className="font-medium text-muted-foreground">
-                Add new Core
-              </div>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <CreateCoreModal withoutWrapperAndTrigger />
+        </Dialog>
       </SidebarMenuItem>
     </SidebarMenu>
   );
